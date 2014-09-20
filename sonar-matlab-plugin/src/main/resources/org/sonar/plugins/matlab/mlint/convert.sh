59335de8bb25c018a07d9298514e4cac11c415d5
Jonathan Martens <jonathan@snetram.nl>
2014-09-20 21:45:43 +0200

    Priority seems to be ordered opposite from my assumption

--- sonar-matlab-plugin/src/main/resources/org/sonar/plugins/matlab/mlint/convert.sh
+++ sonar-matlab-plugin/src/main/resources/org/sonar/plugins/matlab/mlint/convert.sh
@@ -14,14 +14,13 @@ cat $TEMPFILE \
 | sed -e '/^.*\=.*$/d' -e 's/^\s*//' -e 's/ \{2,\}/|/g' \
 | awk -F "|" '{ print "\t<rule>\n\t\t<key>"$1"</key>\n\t\t<configKey>"$1"</configKey>\n\t\t<name>"$1"</name>\n\t\t<priority>"$2"</priority>\n\t\t<description>\n\t\t\t<![CDATA["$3"]]>\n\t\t</description>\n\t</rule>"; }' >> $FILE
 echo "</rules>" >> $FILE
-sed -i 's/>0</>BLOCKER</' $FILE
-sed -i 's/>1</>BLOCKER</' $FILE
-sed -i 's/>2</>CRITICAL</' $FILE
+sed -i 's/>7</>BLOCKER</' $FILE
+sed -i 's/>5</>BLOCKER</' $FILE
+sed -i 's/>4</>CRITICAL</' $FILE
 sed -i 's/>3</>MAJOR</' $FILE
-sed -i 's/>4</>MINOR</' $FILE
-sed -i 's/>5</>INFO</' $FILE
-sed -i 's/>7</>INFO</' $FILE
-
+sed -i 's/>2</>MINOR</' $FILE
+sed -i 's/>1</>INFO</' $FILE
+sed -i 's/>0</>INFO</' $FILE
 
 # Create rules-profile.xml
 echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>" > $PROFILE 
@@ -34,11 +33,12 @@ cat $TEMPFILE \
 | awk -F "|" '{ print "\t\t<rule>\n\t\t\t<repository>MLint<repository>\n\t\t\t<key>"$1"</key>\n\t\t\t<priority>"$2"</priority>\n\t\t</rule>"; }' >> $PROFILE
 echo "	</rules>" >> $PROFILE
 echo "</profile>" >> $PROFILE
-sed -i 's/>0</>BLOCKER</' $PROFILE
-sed -i 's/>1</>BLOCKER</' $PROFILE
-sed -i 's/>2</>CRITICAL</' $PROFILE
+sed -i 's/>7</>BLOCKER</' $PROFILE
+sed -i 's/>5</>BLOCKER</' $PROFILE
+sed -i 's/>4</>CRITICAL</' $PROFILE
 sed -i 's/>3</>MAJOR</' $PROFILE
-sed -i 's/>4</>MINOR</' $PROFILE
-sed -i 's/>5</>INFO</' $PROFILE
-sed -i 's/>7</>INFO</' $PROFILE
+sed -i 's/>2</>MINOR</' $PROFILE
+sed -i 's/>1</>INFO</' $PROFILE
+sed -i 's/>0</>INFO</' $PROFILE
 
+rm -f $TEMPFILE
